c681cd6a7cb34e0c298b0c24dfa34cbfbe1911fd
Vinicius <viniciusmrchede@gmail.com>
2022-06-20 13:25:47 -0300

    teste

---
+++ react/myp/src/App.js
@@ -0,0 +1,18 @@
+import Main from './components/Main'
+import React from 'react';
+
+//import Dashboard from './components/Dashboard';
+
+//import StickyFooter from './components/StickyFooter';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+export default App; 
+ 
+
