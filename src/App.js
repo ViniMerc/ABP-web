766a130d0ee0a0031fd78a3e37258a780d1bd48a
Vinicius <viniciusmrchede@gmail.com>
2022-06-28 10:02:56 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //import Login from './components/Login'
-import Cadastro from './components/Cadastro'
+//import Cadastro from './components/Cadastro'
 
 //import Dashboard from './components/Dashboard/Dashboard'
+
+import Teste from './components/Teste'
 
 function App() {
   return (
     <div className="App">
-      <Cadastro />
+      <Teste />
     </div>
   );
 }
