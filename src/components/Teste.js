e288aa42fde9d2a2a25de070a4e19342dd07541e
Vinicius <viniciusmrchede@gmail.com>
2022-06-28 10:45:10 -0300

    teste

--- src/components/Teste.js
+++ src/components/Teste.js
@@ -1,13 +1,31 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Avatar from '@mui/material/Avatar';
+
+//tabela
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const theme = createTheme();
+
+function createData(name, entrada, saida) {
+    return { name, entrada, saida};
+  }
+
+  const rows = [
+    createData('Vinicius', 13, 6),
+    createData('Vinicius', 19, 6)
+  ];
 
 export default function SignIn() {
   const handleSubmit = (event) => {
@@ -28,45 +46,50 @@ export default function SignIn() {
             marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
+            alignItems: 'center',}}>
 
           <Typography component="h1" variant="h5">
-            Log in
+            Dashboard
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Endereço de email"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Senha"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-           
+          
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            V
+          </Avatar>
+        
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 150 }} size="small" aria-label="a dense table">
+              <TableHead>
+                 <TableRow>
+                   <TableCell>Nome</TableCell>
+                   <TableCell align="left">Entrada</TableCell>
+                   <TableCell align="left">Saida</TableCell>
+                 </TableRow>
+              </TableHead>
+              <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="left">{row.entrada}</TableCell>
+                      <TableCell align="left">{row.saida}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+          </TableContainer>
 
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>              
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Log in
-            </Button>
+              sx={{ mt: 3, mb: 2 }}> Marcar ponto </Button>
           </Box>
+
         </Box>
       </Container>
     </ThemeProvider>
