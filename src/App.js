3ff8a05442e460c881879d43df4e818cacdf216e
Vinicius <viniciusmrchede@gmail.com>
2022-06-29 10:34:31 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 
-import Login from './pages/Login'
-import Cadastro from './pages/Cadastro'
-import Tabela from './pages/Tabela'
+//import Login from './pages/Login'
+//import Cadastro from './pages/Cadastro'
+//import Tabela from './pages/Tabela'
 import Home from './pages/Home'
 
 export default function App() {
